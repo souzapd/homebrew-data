e4df8834c09f46992dba027d6ce07cc5f64da53b
David Souza <32783622+souzapd@users.noreply.github.com>
2021-04-22 10:28:19 -0300

    New: custom R brew formula

---
+++ r-x11.rb
@@ -0,0 +1,112 @@
+class RX11 < Formula
+  desc "Software environment for statistical computing"
+  homepage "https://www.r-project.org/"
+  url "https://cran.r-project.org/src/base/R-4/R-4.0.5.tar.gz"
+  sha256 "0a3ee079aa772e131fe5435311ab627fcbccb5a50cabc54292e6f62046f1ffef"
+  license "GPL-2.0-or-later"
+  revision 1
+  
+  
+  env :std # Enables use of XQuartz headers
+  
+  depends_on "pkg-config" => :build
+  depends_on "gcc" # for gfortran
+  depends_on "gettext"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "openblas"
+  depends_on "pcre2"
+  depends_on "readline"
+  depends_on "tcl-tk"
+  depends_on "xz"
+  
+  # needed to preserve executable permissions on files without shebangs
+  skip_clean "lib/R/bin", "lib/R/doc"
+  
+  def install
+    # BLAS detection fails with Xcode 12 due to missing prototype
+    # https://bugs.r-project.org/bugzilla/show_bug.cgi?id=18024
+    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-memory-profiling",
+      "--with-cairo",
+      "--with-x",
+      "--with-tcl-config=#{Formula["tcl-tk"].opt_lib}/tclConfig.sh",
+      "--with-tk-config=#{Formula["tcl-tk"].opt_lib}/tkConfig.sh",
+      "--with-aqua",
+      "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas",
+      "--with-lapack"
+      "--enable-R-shlib",
+      "--disable-java",
+    ]
+    
+    # Don't remember Homebrew's sed shim
+    args << "SED=/usr/bin/sed" if File.exist?("/usr/bin/sed")
+    
+    # Help CRAN packages find gettext and readline
+    ["gettext", "readline", "xz"].each do |f|
+      ENV.append "CPPFLAGS", "-I#{Formula[f].opt_include}"
+      ENV.append "LDFLAGS", "-L#{Formula[f].opt_lib}"
+    end
+
+    
+    # help picking up x11 cairo
+    ENV.prepend_path "PKG_CONFIG_LIBDIR", "#{MacOS::XQuartz.lib}/pkgconfig"
+    ENV.prepend "CPPFLAGS", "-I#{MacOS::XQuartz.include}"
+    ENV.prepend "LDFLAGS", "-L#{MacOS::XQuartz.lib}"
+    ENV["r_cv_has_pangocairo"] = "no"    
+    
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize do
+      system "make", "install"
+    end
+
+    cd "src/nmath/standalone" do
+      system "make"
+      ENV.deparallelize do
+        system "make", "install"
+      end
+    end
+    
+    r_home = lib/"R"
+    
+    # make Homebrew packages discoverable for R CMD INSTALL
+    inreplace r_home/"etc/Makeconf" do |s|
+      s.gsub!(/^CPPFLAGS =.*/, "\\0 -I#{HOMEBREW_PREFIX}/include")
+      s.gsub!(/^LDFLAGS =.*/, "\\0 -L#{HOMEBREW_PREFIX}/lib")
+      s.gsub!(/.LDFLAGS =.*/, "\\0 $(LDFLAGS)")
+    end
+
+    include.install_symlink Dir[r_home/"include/*"]
+    lib.install_symlink Dir[r_home/"lib/*"]
+
+    # avoid triggering mandatory rebuilds of r when gcc is upgraded
+    inreplace lib/"R/etc/Makeconf", Formula["gcc"].prefix.realpath,
+                                    Formula["gcc"].opt_prefix    
+    
+  end
+  
+  def post_install
+    short_version =
+      `#{bin}/Rscript -e 'cat(as.character(getRversion()[1,1:2]))'`.strip
+    site_library = HOMEBREW_PREFIX/"lib/R/#{short_version}/site-library"
+    site_library.mkpath
+    ln_s site_library, lib/"R/site-library"
+  end
+
+  test do
+    assert_equal "[1] 2", shell_output("#{bin}/Rscript -e 'print(1+1)'").chomp
+    assert_equal ".dylib", shell_output("#{bin}/R CMD config DYLIB_EXT").chomp
+    assert_equal "[1] \"aqua\"",
+                 shell_output("#{bin}/Rscript -e 'library(tcltk)' -e 'tclvalue(.Tcl(\"tk windowingsystem\"))'").chomp
+
+    system bin/"Rscript", "-e", "install.packages('gss', '.', 'https://cloud.r-project.org')"
+    assert_predicate testpath/"gss/libs/gss.so", :exist?,
+                     "Failed to install gss package"
+  end  
+  
+end
