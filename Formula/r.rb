95f8518614472ad001ca33f4e556a55b9236a26d
David Souza <32783622+souzapd@users.noreply.github.com>
2021-04-22 21:16:36 -0300

    Update: Removing =#{frameworks

--- Formula/r.rb
+++ Formula/r.rb
@@ -38,7 +38,7 @@ class RX11 < Formula
       "--with-tk-config=#{Formula["tcl-tk"].opt_lib}/tkConfig.sh",
       "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas",
       "--with-lapack",
-      "--enable-R-framework=#{frameworks}", # To install as a framework. Implies --enable-R-shlib
+      "--enable-R-framework", # To install as a framework. Implies --enable-R-shlib
       # "--enable-R-shlib", # build R as a shared/dynamic library 
       "--disable-java",
     ]
