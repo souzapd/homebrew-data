ad358424f3b933f95274dbe0b2c2666ca5beef97
David Souza <32783622+souzapd@users.noreply.github.com>
2022-01-29 18:42:29 -0300

    Update r.rb

--- r.rb
+++ r.rb
@@ -15,7 +15,6 @@ class R < Formula
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "libtiff"
-  depends_on "libffi" # Used on the lastest brew formula
   depends_on "openblas"
   depends_on "pcre2"
   depends_on "readline"
